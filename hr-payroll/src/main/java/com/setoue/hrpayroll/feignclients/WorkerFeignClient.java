26aaca457c691d12db143ad2bd43c185595d3812
Setoue <proshigeru@gmail.com>
2025-07-04 00:09:07 -0300

    feat: add ribbon load balancing

--- hr-payroll/src/main/java/com/setoue/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/setoue/hrpayroll/feignclients/WorkerFeignClient.java
@@ -7,10 +7,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Optional;
 
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
 
     @GetMapping(value = "/{workerId}")
--- hr-worker/src/main/java/com/setoue/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/setoue/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.setoue.hrworker.resources;
 
 import com.setoue.hrworker.entities.Worker;
 import com.setoue.hrworker.repositories.WorkRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,11 @@ import java.util.Optional;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkRepository repository;
 
@@ -28,6 +36,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{workerId}")
     public ResponseEntity<Worker> findById(@PathVariable Long workerId) {
+
+        logger.info("port =" + env.getProperty("local.server.port"));
+
         Optional<Worker> obj = repository.findById(workerId);
 
         return obj
