8520b89d7e6b8b1ab0325f0008f859ab1eba7afe
Setoue <proshigeru@gmail.com>
2025-07-14 22:39:00 -0300

    feat: add project hr-api-gateway

---
+++ ht-api-gateway-zuul/src/main/java/com/setoue/ht_api_gateway_zuul/HtApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.setoue.ht_api_gateway_zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ ht-api-gateway-zuul/src/test/java/com/setoue/ht_api_gateway_zuul/HtApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setoue.ht_api_gateway_zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
